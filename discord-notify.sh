dc0e92dcbb802dbd8a2ac4c8771a7ff4136e9153
Edward <114024773+medwrdsx@users.noreply.github.com>
2022-12-20 14:38:25 +0800

    bash script

--- discord-notify.sh
+++ discord-notify.sh
@@ -2,11 +2,12 @@
 
 discord_url="https://discord.com/api/webhooks/1052064972210978859/HeAxsPsmMz48s-W-ILTCdnRVYPkuNzTsnMEonOIWbw1HrDZNP0hBHL7EOP78VGWwJwDH"
 
-while read oldrev newrev ref
-do
-	if [[ $ref =~ .*/master$ ]];
-	then
-		git log --pretty=format:'%h - %an, %ar : %s' $oldrev..$newrev > git.log
-		curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"'catgit.log'\"}" $discord_url
-	fi
-done
+generate_post_data() {
+  cat <<EOF
+{
+  "content": "This is a sample message using linux script!",
+}
+EOF
+}
+
+curl -H "Content-Type: application/json" -X POST -d "$(generate_post_data)" $discord_url
