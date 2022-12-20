29dd751051f71e36357cccfee94cecd54142d860
Edward <114024773+medwrdsx@users.noreply.github.com>
2022-12-20 14:29:09 +0800

    update script

--- discord-notify.sh
+++ discord-notify.sh
@@ -2,11 +2,6 @@
 
 discord_url="https://discord.com/api/webhooks/1052064972210978859/HeAxsPsmMz48s-W-ILTCdnRVYPkuNzTsnMEonOIWbw1HrDZNP0hBHL7EOP78VGWwJwDH"
 
-generate_post_data() {
-  cat <<EOF
-{
-  "content": "@everyone **Failed Unit**!"
- 
 while read oldrev newrev ref
 do
 	if [[ $ref =~ .*/master$ ]];
@@ -15,9 +10,3 @@ do
 		curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"'catgit.log'\"}" $discord_url
 	fi
 done
-
-}
-EOF
-}
-
-curl -H "Content-Type: application/json" -X POST -d "$(generate_post_data)" $discord_url
